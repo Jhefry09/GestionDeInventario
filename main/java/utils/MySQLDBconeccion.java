c3f7830d213f039134da7de5cde8a44de3eda956
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-03 20:44:38 -0500

    coneccion a la base de datos

---
+++ main/java/utils/MySQLDBconeccion.java
@@ -0,0 +1,17 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MySQLDBconeccion {
+public static Connection getConexion() {
+	Connection cn = null;
+	try {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/GestionDeInventario", "root", "030609");
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+	return cn;
+}
+}
