898deee77cfbd5787aab2f8a6ce02ff7d4325180
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-05 00:28:35 -0500

    cambio en el nombre ya que habia un error en la palabra administrador

---
+++ main/java/controlador/ServletAdministrador.java
@@ -0,0 +1,46 @@
+package controlador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import entidad.Administrador;
+import modelo.ModelAdministrador;
+
+/**
+ * Servlet implementation class ServletAministrador
+ */
+@WebServlet("/ServletAdministrador")
+public class ServletAdministrador extends HttpServlet {
+	ModelAdministrador mat = new ModelAdministrador(); 
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletAdministrador() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	String login, clave;
+	login = request.getParameter("txtUsuario");
+	clave = request.getParameter("txtClave");
+	Administrador admin = mat.iniciarSesion(login, clave);
+	if(admin == null) {
+		request.setAttribute("error", "clave/usuario incorrecto");
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+		System.out.println("fallo contraseña");
+	}else {
+		response.sendRedirect("ServletInventario?seleccion=Mostrar");
+		System.out.println("Inicio de sesion correcto");
+	}
+	}
+}
