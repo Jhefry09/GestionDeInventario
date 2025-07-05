a7fcde70c7fad7242a9d0d3971e4d1a56c6a79a5
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-04 23:16:20 -0500

    creacion de la entidad Administrador (getters y setters)

---
+++ main/java/entidad/Administrador.java
@@ -0,0 +1,36 @@
+package entidad;
+
+public class Administrador {
+private String nombre_administrador, apellido_administrador, login_administrador, contraseña_administrador;
+private Integer id_administrador;
+public String getNombre_administrador() {
+	return nombre_administrador;
+}
+public void setNombre_administrador(String nombre_administrador) {
+	this.nombre_administrador = nombre_administrador;
+}
+public String getApellido_administrador() {
+	return apellido_administrador;
+}
+public void setApellido_administrador(String apellido_administrador) {
+	this.apellido_administrador = apellido_administrador;
+}
+public String getLogin_administrador() {
+	return login_administrador;
+}
+public void setLogin_administrador(String login_administrador) {
+	this.login_administrador = login_administrador;
+}
+public String getContraseña_administrador() {
+	return contraseña_administrador;
+}
+public void setContraseña_administrador(String contraseña_administrador) {
+	this.contraseña_administrador = contraseña_administrador;
+}
+public Integer getId_administrador() {
+	return id_administrador;
+}
+public void setId_administrador(Integer id_administrador) {
+	this.id_administrador = id_administrador;
+}
+}
