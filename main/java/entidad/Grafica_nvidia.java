d2f54ef5f0983b75fcc880f08612fc23a0aad0c1
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-07 02:12:19 -0500

    creacion de la entidad Grafica_nvidia con sus getters y setters

---
+++ main/java/entidad/Grafica_nvidia.java
@@ -0,0 +1,48 @@
+package entidad;
+
+public class Grafica_nvidia {
+	private int  id_grafica_nvidia, generacion_grafi_nvidia_id, gama_grafi_nvidia_id, cate_id, stock_grafi_nvidia, precio_grafi_nvidia;
+	private String  descripcion_grafi_nvidia;
+	public int getId_grafica_nvidia() {
+		return id_grafica_nvidia;
+	}
+	public void setId_grafica_nvidia(int id_grafica_nvidia) {
+		this.id_grafica_nvidia = id_grafica_nvidia;
+	}
+	public int getGeneracion_grafi_nvidia_id() {
+		return generacion_grafi_nvidia_id;
+	}
+	public void setGeneracion_grafi_nvidia_id(int generacion_grafi_nvidia_id) {
+		this.generacion_grafi_nvidia_id = generacion_grafi_nvidia_id;
+	}
+	public int getGama_grafi_nvidia_id() {
+		return gama_grafi_nvidia_id;
+	}
+	public void setGama_grafi_nvidia_id(int gama_grafi_nvidia_id) {
+		this.gama_grafi_nvidia_id = gama_grafi_nvidia_id;
+	}
+	public int getCate_id() {
+		return cate_id;
+	}
+	public void setCate_id(int cate_id) {
+		this.cate_id = cate_id;
+	}
+	public int getStock_grafi_nvidia() {
+		return stock_grafi_nvidia;
+	}
+	public void setStock_grafi_nvidia(int stock_grafi_nvidia) {
+		this.stock_grafi_nvidia = stock_grafi_nvidia;
+	}
+	public int getPrecio_grafi_nvidia() {
+		return precio_grafi_nvidia;
+	}
+	public void setPrecio_grafi_nvidia(int precio_grafi_nvidia) {
+		this.precio_grafi_nvidia = precio_grafi_nvidia;
+	}
+	public String getDescripcion_grafi_nvidia() {
+		return descripcion_grafi_nvidia;
+	}
+	public void setDescripcion_grafi_nvidia(String descripcion_grafi_nvidia) {
+		this.descripcion_grafi_nvidia = descripcion_grafi_nvidia;
+	}
+}
