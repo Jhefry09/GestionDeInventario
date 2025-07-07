228581a62058080a9e8656f985ce637a28ee5b3d
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-07 02:09:07 -0500

    creacion de la entidad Gama_intel con sus getters y setters

---
+++ main/java/entidad/Gama_intel.java
@@ -0,0 +1,18 @@
+package entidad;
+
+public class Gama_intel {
+	private int  id_gama_intel;
+	private String  nom_gama_intel;
+	public int getId_gama_intel() {
+		return id_gama_intel;
+	}
+	public void setId_gama_intel(int id_gama_intel) {
+		this.id_gama_intel = id_gama_intel;
+	}
+	public String getNom_gama_intel() {
+		return nom_gama_intel;
+	}
+	public void setNom_gama_intel(String nom_gama_intel) {
+		this.nom_gama_intel = nom_gama_intel;
+	}
+}
