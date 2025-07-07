4f1119f53742296a0447db254f03bfa3dd1cea96
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-06 22:23:54 -0500

    creacion de la entidad PERIFERICOS con sus getters y setters

---
+++ main/java/entidad/Perifericos.java
@@ -0,0 +1,36 @@
+package entidad;
+
+public class Perifericos {
+private int id_perifericos, tipo_perifericos_id, precio_perifericos, cate_id;
+private String descripc_perifericos;
+public int getId_perifericos() {
+	return id_perifericos;
+}
+public void setId_perifericos(int id_perifericos) {
+	this.id_perifericos = id_perifericos;
+}
+public int getTipo_perifericos_id() {
+	return tipo_perifericos_id;
+}
+public void setTipo_perifericos_id(int tipo_perifericos_id) {
+	this.tipo_perifericos_id = tipo_perifericos_id;
+}
+public int getPrecio_perifericos() {
+	return precio_perifericos;
+}
+public void setPrecio_perifericos(int precio_perifericos) {
+	this.precio_perifericos = precio_perifericos;
+}
+public int getCate_id() {
+	return cate_id;
+}
+public void setCate_id(int cate_id) {
+	this.cate_id = cate_id;
+}
+public String getDescripc_perifericos() {
+	return descripc_perifericos;
+}
+public void setDescripc_perifericos(String descripc_perifericos) {
+	this.descripc_perifericos = descripc_perifericos;
+}
+}
