c0d4d93b592f0a11f8bd6a723317b2b2afac4e28
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-07 02:12:56 -0500

    creacion de la entidad Perifericos_tipo con sus getters y setters

---
+++ main/java/entidad/Perifericos_tipo.java
@@ -0,0 +1,18 @@
+package entidad;
+
+public class Perifericos_tipo {
+	private int  id_tipo_perifericos;
+	private String descrip_tipo_perifericos;
+	public int getId_tipo_perifericos() {
+		return id_tipo_perifericos;
+	}
+	public void setId_tipo_perifericos(int id_tipo_perifericos) {
+		this.id_tipo_perifericos = id_tipo_perifericos;
+	}
+	public String getDescrip_tipo_perifericos() {
+		return descrip_tipo_perifericos;
+	}
+	public void setDescrip_tipo_perifericos(String descrip_tipo_perifericos) {
+		this.descrip_tipo_perifericos = descrip_tipo_perifericos;
+	}
+}
