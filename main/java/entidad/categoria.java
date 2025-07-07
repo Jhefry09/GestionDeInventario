a8e187b44cce8982468d70c517051c5836e4222f
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-06 22:56:35 -0500

    creacion de la entidad CATEGORIA con sus getters y setters

---
+++ main/java/entidad/categoria.java
@@ -0,0 +1,18 @@
+package entidad;
+
+public class categoria {
+private int id_categoria;
+private String nom_cate;
+public int getId_categoria() {
+	return id_categoria;
+}
+public void setId_categoria(int id_categoria) {
+	this.id_categoria = id_categoria;
+}
+public String getNom_cate() {
+	return nom_cate;
+}
+public void setNom_cate(String nom_cate) {
+	this.nom_cate = nom_cate;
+}
+}
