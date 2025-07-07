809b5b09181da0d0b53b23ce96fe96becf84cf96
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-07 02:09:43 -0500

    creacion de la entidad Gama_proce_amd con sus getters y setters

---
+++ main/java/entidad/Gama_proce_amd.java
@@ -0,0 +1,18 @@
+package entidad;
+
+public class Gama_proce_amd {
+	private int  id_gama_proce_amd;
+	private String  nom_gama_proc_amd;
+	public int getId_gama_proce_amd() {
+		return id_gama_proce_amd;
+	}
+	public void setId_gama_proce_amd(int id_gama_proce_amd) {
+		this.id_gama_proce_amd = id_gama_proce_amd;
+	}
+	public String getNom_gama_proc_amd() {
+		return nom_gama_proc_amd;
+	}
+	public void setNom_gama_proc_amd(String nom_gama_proc_amd) {
+		this.nom_gama_proc_amd = nom_gama_proc_amd;
+	}
+}
