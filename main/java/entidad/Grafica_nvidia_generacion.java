ea2ed1c558644e5fca248421b5e45e8e56ebdfb7
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-07 02:12:04 -0500

    creacion de la entidad Grafica_nvidia_generacion con sus getters y setters

---
+++ main/java/entidad/Grafica_nvidia_generacion.java
@@ -0,0 +1,21 @@
+package entidad;
+
+public class Grafica_nvidia_generacion {
+	private int  id_gene_grafi_nvidia, descrip_gene_grafi_nvidia;
+
+	public int getId_gene_grafi_nvidia() {
+		return id_gene_grafi_nvidia;
+	}
+
+	public void setId_gene_grafi_nvidia(int id_gene_grafi_nvidia) {
+		this.id_gene_grafi_nvidia = id_gene_grafi_nvidia;
+	}
+
+	public int getDescrip_gene_grafi_nvidia() {
+		return descrip_gene_grafi_nvidia;
+	}
+
+	public void setDescrip_gene_grafi_nvidia(int descrip_gene_grafi_nvidia) {
+		this.descrip_gene_grafi_nvidia = descrip_gene_grafi_nvidia;
+	}
+}
