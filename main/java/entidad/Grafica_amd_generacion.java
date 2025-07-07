69e7c1fb002918844ac700c6d767cc34397acd83
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-07 02:11:07 -0500

    creacion de la entidad Grafica_amd_generacion con sus getters y setters

---
+++ main/java/entidad/Grafica_amd_generacion.java
@@ -0,0 +1,18 @@
+package entidad;
+
+public class Grafica_amd_generacion {
+	private int  id_gene_grafi_amd;
+	private String  descrip_gene_grafi_amd;
+	public int getId_gene_grafi_amd() {
+		return id_gene_grafi_amd;
+	}
+	public void setId_gene_grafi_amd(int id_gene_grafi_amd) {
+		this.id_gene_grafi_amd = id_gene_grafi_amd;
+	}
+	public String getDescrip_gene_grafi_amd() {
+		return descrip_gene_grafi_amd;
+	}
+	public void setDescrip_gene_grafi_amd(String descrip_gene_grafi_amd) {
+		this.descrip_gene_grafi_amd = descrip_gene_grafi_amd;
+	}
+}
