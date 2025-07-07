7f7be4b112e5bbcf83196cb8f2e031fe91cb6ed5
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-07 02:10:53 -0500

    creacion de la entidad Grafica_amd_gama con sus getters y setters

---
+++ main/java/entidad/Grafica_amd_gama.java
@@ -0,0 +1,18 @@
+package entidad;
+
+public class Grafica_amd_gama {
+	private int  id_gama_amd_grafica;
+	private String  gama_descrip;
+	public int getId_gama_amd_grafica() {
+		return id_gama_amd_grafica;
+	}
+	public void setId_gama_amd_grafica(int id_gama_amd_grafica) {
+		this.id_gama_amd_grafica = id_gama_amd_grafica;
+	}
+	public String getGama_descrip() {
+		return gama_descrip;
+	}
+	public void setGama_descrip(String gama_descrip) {
+		this.gama_descrip = gama_descrip;
+	}
+}
