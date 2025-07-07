ee042c12ce94df7391e13240a620aacb79841d3a
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-07 02:07:11 -0500

    creacion de la entidad Almacenamiento_unidades con sus getters y setters

---
+++ main/java/entidad/Almacenamiento_unidades.java
@@ -0,0 +1,18 @@
+package entidad;
+
+public class Almacenamiento_unidades {
+private int id_unid;
+private String nombre_unid;
+public int getId_unid() {
+	return id_unid;
+}
+public void setId_unid(int id_unid) {
+	this.id_unid = id_unid;
+}
+public String getNombre_unid() {
+	return nombre_unid;
+}
+public void setNombre_unid(String nombre_unid) {
+	this.nombre_unid = nombre_unid;
+}
+}
