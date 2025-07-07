30f61d08b8e657a4a17c09bba7700af3d69c1927
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-07 02:14:40 -0500

    creacion de la entidad Ram_generacion con sus getters y setters

---
+++ main/java/entidad/Ram_generacion.java
@@ -0,0 +1,18 @@
+package entidad;
+
+public class Ram_generacion {
+	private int  id_gener_ram_nom;
+	private String  gener_ram_nom;
+	public int getId_gener_ram_nom() {
+		return id_gener_ram_nom;
+	}
+	public void setId_gener_ram_nom(int id_gener_ram_nom) {
+		this.id_gener_ram_nom = id_gener_ram_nom;
+	}
+	public String getGener_ram_nom() {
+		return gener_ram_nom;
+	}
+	public void setGener_ram_nom(String gener_ram_nom) {
+		this.gener_ram_nom = gener_ram_nom;
+	}
+}
