382a1fd844f6a505edd797729a072496eeaf9b46
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-07 02:08:09 -0500

    creacion de la entidad Fuente_capacidad con sus getters y setters

---
+++ main/java/entidad/Fuente_capacidad.java
@@ -0,0 +1,18 @@
+package entidad;
+
+public class Fuente_capacidad {
+	private int  id_capacidad_fuente;
+	private String  watts;
+	public int getId_capacidad_fuente() {
+		return id_capacidad_fuente;
+	}
+	public void setId_capacidad_fuente(int id_capacidad_fuente) {
+		this.id_capacidad_fuente = id_capacidad_fuente;
+	}
+	public String getWatts() {
+		return watts;
+	}
+	public void setWatts(String watts) {
+		this.watts = watts;
+	}
+}
