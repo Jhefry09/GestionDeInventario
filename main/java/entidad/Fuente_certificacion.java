1fe7a0a1485932a4d36972f8a9f28191f4f986c5
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-07 02:08:26 -0500

    creacion de la entidad Fuente_certificacion con sus getters y setters

---
+++ main/java/entidad/Fuente_certificacion.java
@@ -0,0 +1,18 @@
+package entidad;
+
+public class Fuente_certificacion {
+	private int  id_fuente_certificacion;
+	private String  certificacion;
+	public int getId_fuente_certificacion() {
+		return id_fuente_certificacion;
+	}
+	public void setId_fuente_certificacion(int id_fuente_certificacion) {
+		this.id_fuente_certificacion = id_fuente_certificacion;
+	}
+	public String getCertificacion() {
+		return certificacion;
+	}
+	public void setCertificacion(String certificacion) {
+		this.certificacion = certificacion;
+	}
+}
