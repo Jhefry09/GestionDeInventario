2d0daf9aaac41962ec9b975fc0a30c1c7a6d2efa
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-07 02:14:24 -0500

    creacion de la entidad Ram_capacidad con sus getters y setters

---
+++ main/java/entidad/Ram_capacidad.java
@@ -0,0 +1,21 @@
+package entidad;
+
+public class Ram_capacidad {
+	private int  id_ram_capac, capacidad_gb;
+
+	public int getId_ram_capac() {
+		return id_ram_capac;
+	}
+
+	public void setId_ram_capac(int id_ram_capac) {
+		this.id_ram_capac = id_ram_capac;
+	}
+
+	public int getCapacidad_gb() {
+		return capacidad_gb;
+	}
+
+	public void setCapacidad_gb(int capacidad_gb) {
+		this.capacidad_gb = capacidad_gb;
+	}
+}
