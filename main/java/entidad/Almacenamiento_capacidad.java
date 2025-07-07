cec1467f307a09ef35824e11ad7b56e70535fcce
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-07 02:06:34 -0500

    creacion de la entidad Almacenamiento_capacidad con sus getters y setters

---
+++ main/java/entidad/Almacenamiento_capacidad.java
@@ -0,0 +1,29 @@
+package entidad;
+
+public class Almacenamiento_capacidad {
+private int id_capaci_almac, desc_capac_almac,uni_almac_id;
+
+public int getId_capaci_almac() {
+	return id_capaci_almac;
+}
+
+public void setId_capaci_almac(int id_capaci_almac) {
+	this.id_capaci_almac = id_capaci_almac;
+}
+
+public int getDesc_capac_almac() {
+	return desc_capac_almac;
+}
+
+public void setDesc_capac_almac(int desc_capac_almac) {
+	this.desc_capac_almac = desc_capac_almac;
+}
+
+public int getUni_almac_id() {
+	return uni_almac_id;
+}
+
+public void setUni_almac_id(int uni_almac_id) {
+	this.uni_almac_id = uni_almac_id;
+}
+}
