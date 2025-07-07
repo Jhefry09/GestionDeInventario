46ae6bea71956deedd7e4ec95727f3c52b231399
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-07 02:06:54 -0500

    creacion de la entidad Almacenamiento_tipo con sus getters y setters

---
+++ main/java/entidad/Almacenamiento_tipo.java
@@ -0,0 +1,18 @@
+package entidad;
+
+public class Almacenamiento_tipo {
+private int id_almacenamiento_tipo;
+private String desc_tipo_almac;
+public int getId_almacenamiento_tipo() {
+	return id_almacenamiento_tipo;
+}
+public void setId_almacenamiento_tipo(int id_almacenamiento_tipo) {
+	this.id_almacenamiento_tipo = id_almacenamiento_tipo;
+}
+public String getDesc_tipo_almac() {
+	return desc_tipo_almac;
+}
+public void setDesc_tipo_almac(String desc_tipo_almac) {
+	this.desc_tipo_almac = desc_tipo_almac;
+}
+}
