cdf178b052db788d38ae261472c1550d71f2d533
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-07 02:14:52 -0500

    creacion de la entidad Ram con sus getters y setters

---
+++ main/java/entidad/Ram.java
@@ -0,0 +1,42 @@
+package entidad;
+
+public class Ram {
+	private int  id_ram, capaci_ram_id, genera_ram_id, cate_id, precio_ram;
+	private String  descrip_ram;
+	public int getId_ram() {
+		return id_ram;
+	}
+	public void setId_ram(int id_ram) {
+		this.id_ram = id_ram;
+	}
+	public int getCapaci_ram_id() {
+		return capaci_ram_id;
+	}
+	public void setCapaci_ram_id(int capaci_ram_id) {
+		this.capaci_ram_id = capaci_ram_id;
+	}
+	public int getGenera_ram_id() {
+		return genera_ram_id;
+	}
+	public void setGenera_ram_id(int genera_ram_id) {
+		this.genera_ram_id = genera_ram_id;
+	}
+	public int getCate_id() {
+		return cate_id;
+	}
+	public void setCate_id(int cate_id) {
+		this.cate_id = cate_id;
+	}
+	public int getPrecio_ram() {
+		return precio_ram;
+	}
+	public void setPrecio_ram(int precio_ram) {
+		this.precio_ram = precio_ram;
+	}
+	public String getDescrip_ram() {
+		return descrip_ram;
+	}
+	public void setDescrip_ram(String descrip_ram) {
+		this.descrip_ram = descrip_ram;
+	}
+}
