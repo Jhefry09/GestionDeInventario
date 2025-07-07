0e296ae83a4746d242f31094835dfadd334c76fd
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-07 02:07:48 -0500

    creacion de la entidad Disipador_tipo con sus getters y setters

---
+++ main/java/entidad/Disipador_tipo.java
@@ -0,0 +1,18 @@
+package entidad;
+
+public class Disipador_tipo {
+	private int  id_disip_tipo;
+	private String  tipo_disipador;
+	public int getId_disip_tipo() {
+		return id_disip_tipo;
+	}
+	public void setId_disip_tipo(int id_disip_tipo) {
+		this.id_disip_tipo = id_disip_tipo;
+	}
+	public String getTipo_disipador() {
+		return tipo_disipador;
+	}
+	public void setTipo_disipador(String tipo_disipador) {
+		this.tipo_disipador = tipo_disipador;
+	}
+}
