f35656cad510cc6e0cb05a8e7ea587ecda62d6ac
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-07 02:10:07 -0500

    creacion de la entidad Generacion_proce_amd con sus getters y setters

---
+++ main/java/entidad/Generacion_proces_amd.java
@@ -0,0 +1,24 @@
+package entidad;
+
+public class Generacion_proces_amd {
+	private int  id_generacion_proce_amd;
+	private String  nom_gene_proce_amd, nom_clave_gene_amd;
+	public int getId_generacion_proce_amd() {
+		return id_generacion_proce_amd;
+	}
+	public void setId_generacion_proce_amd(int id_generacion_proce_amd) {
+		this.id_generacion_proce_amd = id_generacion_proce_amd;
+	}
+	public String getNom_gene_proce_amd() {
+		return nom_gene_proce_amd;
+	}
+	public void setNom_gene_proce_amd(String nom_gene_proce_amd) {
+		this.nom_gene_proce_amd = nom_gene_proce_amd;
+	}
+	public String getNom_clave_gene_amd() {
+		return nom_clave_gene_amd;
+	}
+	public void setNom_clave_gene_amd(String nom_clave_gene_amd) {
+		this.nom_clave_gene_amd = nom_clave_gene_amd;
+	}
+}
