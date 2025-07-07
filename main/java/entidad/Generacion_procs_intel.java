aa7c30746494e7608d5e48c8a20f38d731a2a88d
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-07 02:10:23 -0500

    creacion de la entidad Generacion_proce_intel con sus getters y setters

---
+++ main/java/entidad/Generacion_procs_intel.java
@@ -0,0 +1,24 @@
+package entidad;
+
+public class Generacion_procs_intel {
+	private int  id_gene_proc_intel;
+	private String  nombre_gene_intel, nombre_clave_gene_intel;
+	public int getId_gene_proc_intel() {
+		return id_gene_proc_intel;
+	}
+	public void setId_gene_proc_intel(int id_gene_proc_intel) {
+		this.id_gene_proc_intel = id_gene_proc_intel;
+	}
+	public String getNombre_gene_intel() {
+		return nombre_gene_intel;
+	}
+	public void setNombre_gene_intel(String nombre_gene_intel) {
+		this.nombre_gene_intel = nombre_gene_intel;
+	}
+	public String getNombre_clave_gene_intel() {
+		return nombre_clave_gene_intel;
+	}
+	public void setNombre_clave_gene_intel(String nombre_clave_gene_intel) {
+		this.nombre_clave_gene_intel = nombre_clave_gene_intel;
+	}
+}
