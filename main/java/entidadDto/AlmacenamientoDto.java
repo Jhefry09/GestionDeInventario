015f951787834e6702e7de812d753b235d6fddf8
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-07 20:02:39 -0500

    creacion de la entidad AlmacenamientoDto con sus getters y setters para poder mostrar los datos de las tablas con Inner Join

---
+++ main/java/entidadDto/AlmacenamientoDto.java
@@ -0,0 +1,48 @@
+package entidadDto;
+
+public class AlmacenamientoDto {
+private String Desc_almace_dto, unidades_almace_dto, tipo_almace_dto;
+private Integer id_almace_dto, capacidad_almace_dto, precio_almace_dto, stock_almace_dto;
+public String getDesc_almace_dto() {
+	return Desc_almace_dto;
+}
+public void setDesc_almace_dto(String desc_almace_dto) {
+	Desc_almace_dto = desc_almace_dto;
+}
+public String getUnidades_almace_dto() {
+	return unidades_almace_dto;
+}
+public void setUnidades_almace_dto(String unidades_almace_dto) {
+	this.unidades_almace_dto = unidades_almace_dto;
+}
+public String getTipo_almace_dto() {
+	return tipo_almace_dto;
+}
+public void setTipo_almace_dto(String tipo_almace_dto) {
+	this.tipo_almace_dto = tipo_almace_dto;
+}
+public Integer getId_almace_dto() {
+	return id_almace_dto;
+}
+public void setId_almace_dto(Integer id_almace_dto) {
+	this.id_almace_dto = id_almace_dto;
+}
+public Integer getCapacidad_almace_dto() {
+	return capacidad_almace_dto;
+}
+public void setCapacidad_almace_dto(Integer capacidad_almace_dto) {
+	this.capacidad_almace_dto = capacidad_almace_dto;
+}
+public Integer getPrecio_almace_dto() {
+	return precio_almace_dto;
+}
+public void setPrecio_almace_dto(Integer precio_almace_dto) {
+	this.precio_almace_dto = precio_almace_dto;
+}
+public Integer getStock_almace_dto() {
+	return stock_almace_dto;
+}
+public void setStock_almace_dto(Integer stock_almace_dto) {
+	this.stock_almace_dto = stock_almace_dto;
+}
+}
