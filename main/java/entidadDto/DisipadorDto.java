d8c568176024d5879b335bfc3fcaedb6dbf0e6b6
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-08 22:11:27 -0500

    creacion de la entidad disipador dto

---
+++ main/java/entidadDto/DisipadorDto.java
@@ -0,0 +1,36 @@
+package entidadDto;
+
+public class DisipadorDto {
+private Integer id_disipador_tdo, precio_disipador_tdo, stock_disipador_tdo;
+private String des_disipador_tdo, tipo_disipador_tdo;
+public Integer getId_disipador_tdo() {
+	return id_disipador_tdo;
+}
+public void setId_disipador_tdo(Integer id_disipador_tdo) {
+	this.id_disipador_tdo = id_disipador_tdo;
+}
+public Integer getPrecio_disipador_tdo() {
+	return precio_disipador_tdo;
+}
+public void setPrecio_disipador_tdo(Integer precio_disipador_tdo) {
+	this.precio_disipador_tdo = precio_disipador_tdo;
+}
+public Integer getStock_disipador_tdo() {
+	return stock_disipador_tdo;
+}
+public void setStock_disipador_tdo(Integer stock_disipador_tdo) {
+	this.stock_disipador_tdo = stock_disipador_tdo;
+}
+public String getDes_disipador_tdo() {
+	return des_disipador_tdo;
+}
+public void setDes_disipador_tdo(String des_disipador_tdo) {
+	this.des_disipador_tdo = des_disipador_tdo;
+}
+public String getTipo_disipador_tdo() {
+	return tipo_disipador_tdo;
+}
+public void setTipo_disipador_tdo(String tipo_disipador_tdo) {
+	this.tipo_disipador_tdo = tipo_disipador_tdo;
+}
+}
