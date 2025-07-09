241f6c4d566a6f3034cfdb79fb6715327b070ba2
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-08 22:10:51 -0500

    creacion de la entidad amdgrafica dto

---
+++ main/java/entidadDto/AmdGraficaDto.java
@@ -0,0 +1,42 @@
+package entidadDto;
+
+public class AmdGraficaDto {
+private Integer id_amd_grafica, gama_amd_grafica, precio_amd_grafica, stock_amd_grafica;
+private String descripcion_amd_grafica, generacion_amd_grafica;
+public Integer getId_amd_grafica() {
+	return id_amd_grafica;
+}
+public void setId_amd_grafica(Integer id_amd_grafica) {
+	this.id_amd_grafica = id_amd_grafica;
+}
+public Integer getGama_amd_grafica() {
+	return gama_amd_grafica;
+}
+public void setGama_amd_grafica(Integer gama_amd_grafica) {
+	this.gama_amd_grafica = gama_amd_grafica;
+}
+public Integer getPrecio_amd_grafica() {
+	return precio_amd_grafica;
+}
+public void setPrecio_amd_grafica(Integer precio_amd_grafica) {
+	this.precio_amd_grafica = precio_amd_grafica;
+}
+public Integer getStock_amd_grafica() {
+	return stock_amd_grafica;
+}
+public void setStock_amd_grafica(Integer stock_amd_grafica) {
+	this.stock_amd_grafica = stock_amd_grafica;
+}
+public String getDescripcion_amd_grafica() {
+	return descripcion_amd_grafica;
+}
+public void setDescripcion_amd_grafica(String descripcion_amd_grafica) {
+	this.descripcion_amd_grafica = descripcion_amd_grafica;
+}
+public String getGeneracion_amd_grafica() {
+	return generacion_amd_grafica;
+}
+public void setGeneracion_amd_grafica(String generacion_amd_grafica) {
+	this.generacion_amd_grafica = generacion_amd_grafica;
+}
+}
