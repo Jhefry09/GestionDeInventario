ba7c7b12ef0dcee8ac79fd8eb2ddd81ae9a6f858
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-08 22:12:45 -0500

    creacion de la entidad NvidiaGrafica dto

---
+++ main/java/entidadDto/NvidiaGraficaDto.java
@@ -0,0 +1,42 @@
+package entidadDto;
+
+public class NvidiaGraficaDto {
+	private Integer id_nvidia_grafica, gama_nvidia_grafica, precio_nvidia_grafica, stock_nvidia_grafica;
+	private String descripcion_nvidia_grafica, generacion_nvidia_grafica;
+	public Integer getId_nvidia_grafica() {
+		return id_nvidia_grafica;
+	}
+	public void setId_nvidia_grafica(Integer id_nvidia_grafica) {
+		this.id_nvidia_grafica = id_nvidia_grafica;
+	}
+	public Integer getGama_nvidia_grafica() {
+		return gama_nvidia_grafica;
+	}
+	public void setGama_nvidia_grafica(Integer gama_nvidia_grafica) {
+		this.gama_nvidia_grafica = gama_nvidia_grafica;
+	}
+	public Integer getPrecio_nvidia_grafica() {
+		return precio_nvidia_grafica;
+	}
+	public void setPrecio_nvidia_grafica(Integer precio_nvidia_grafica) {
+		this.precio_nvidia_grafica = precio_nvidia_grafica;
+	}
+	public Integer getStock_nvidia_grafica() {
+		return stock_nvidia_grafica;
+	}
+	public void setStock_nvidia_grafica(Integer stock_nvidia_grafica) {
+		this.stock_nvidia_grafica = stock_nvidia_grafica;
+	}
+	public String getDescripcion_nvidia_grafica() {
+		return descripcion_nvidia_grafica;
+	}
+	public void setDescripcion_nvidia_grafica(String descripcion_nvidia_grafica) {
+		this.descripcion_nvidia_grafica = descripcion_nvidia_grafica;
+	}
+	public String getGeneracion_nvidia_grafica() {
+		return generacion_nvidia_grafica;
+	}
+	public void setGeneracion_nvidia_grafica(String generacion_nvidia_grafica) {
+		this.generacion_nvidia_grafica = generacion_nvidia_grafica;
+	}
+}
