0530cd589eb5e5a0197ac8b144e498406664ba58
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-08 22:12:27 -0500

    creacion de la entidad IntelProcesador dto

---
+++ main/java/entidadDto/IntelProcesadorDto.java
@@ -0,0 +1,42 @@
+package entidadDto;
+
+public class IntelProcesadorDto {
+private Integer id_intel, precio_intel, stock_intel;
+private String gama_intel, generacion_intel, nombre_clave_intel;
+public Integer getId_intel() {
+	return id_intel;
+}
+public void setId_intel(Integer id_intel) {
+	this.id_intel = id_intel;
+}
+public Integer getPrecio_intel() {
+	return precio_intel;
+}
+public void setPrecio_intel(Integer precio_intel) {
+	this.precio_intel = precio_intel;
+}
+public Integer getStock_intel() {
+	return stock_intel;
+}
+public void setStock_intel(Integer stock_intel) {
+	this.stock_intel = stock_intel;
+}
+public String getGama_intel() {
+	return gama_intel;
+}
+public void setGama_intel(String gama_intel) {
+	this.gama_intel = gama_intel;
+}
+public String getGeneracion_intel() {
+	return generacion_intel;
+}
+public void setGeneracion_intel(String generacion_intel) {
+	this.generacion_intel = generacion_intel;
+}
+public String getNombre_clave_intel() {
+	return nombre_clave_intel;
+}
+public void setNombre_clave_intel(String nombre_clave_intel) {
+	this.nombre_clave_intel = nombre_clave_intel;
+}
+}
