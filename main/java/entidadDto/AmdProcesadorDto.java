056568297876e24c5e223755ae79b41c908c4707
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-08 22:11:09 -0500

    creacion de la entidad amdProcesador dto

---
+++ main/java/entidadDto/AmdProcesadorDto.java
@@ -0,0 +1,36 @@
+package entidadDto;
+
+public class AmdProcesadorDto {
+private Integer id_amd_procesador, precio_amd_procesador, stock_amd_procesador;
+private String gama_amd_procesador, nombre_clave_amd_procesador;
+public Integer getId_amd_procesador() {
+	return id_amd_procesador;
+}
+public void setId_amd_procesador(Integer id_amd_procesador) {
+	this.id_amd_procesador = id_amd_procesador;
+}
+public Integer getPrecio_amd_procesador() {
+	return precio_amd_procesador;
+}
+public void setPrecio_amd_procesador(Integer precio_amd_procesador) {
+	this.precio_amd_procesador = precio_amd_procesador;
+}
+public Integer getStock_amd_procesador() {
+	return stock_amd_procesador;
+}
+public void setStock_amd_procesador(Integer stock_amd_procesador) {
+	this.stock_amd_procesador = stock_amd_procesador;
+}
+public String getGama_amd_procesador() {
+	return gama_amd_procesador;
+}
+public void setGama_amd_procesador(String gama_amd_procesador) {
+	this.gama_amd_procesador = gama_amd_procesador;
+}
+public String getNombre_clave_amd_procesador() {
+	return nombre_clave_amd_procesador;
+}
+public void setNombre_clave_amd_procesador(String nombre_clave_amd_procesador) {
+	this.nombre_clave_amd_procesador = nombre_clave_amd_procesador;
+}
+}
