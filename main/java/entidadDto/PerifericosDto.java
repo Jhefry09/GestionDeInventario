ddc3ddedf3ddc9e542a7d15e6ac8cbbfcc12745b
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-08 22:13:01 -0500

    creacion de la entidad Perifericos dto

---
+++ main/java/entidadDto/PerifericosDto.java
@@ -0,0 +1,36 @@
+package entidadDto;
+
+public class PerifericosDto {
+private Integer id_perifericos, precio_perifericos, stock_perifericos;
+private String descripcion_perifericos, tipo_perifericos;
+public Integer getId_perifericos() {
+	return id_perifericos;
+}
+public void setId_perifericos(Integer id_perifericos) {
+	this.id_perifericos = id_perifericos;
+}
+public Integer getPrecio_perifericos() {
+	return precio_perifericos;
+}
+public void setPrecio_perifericos(Integer precio_perifericos) {
+	this.precio_perifericos = precio_perifericos;
+}
+public Integer getStock_perifericos() {
+	return stock_perifericos;
+}
+public void setStock_perifericos(Integer stock_perifericos) {
+	this.stock_perifericos = stock_perifericos;
+}
+public String getDescripcion_perifericos() {
+	return descripcion_perifericos;
+}
+public void setDescripcion_perifericos(String descripcion_perifericos) {
+	this.descripcion_perifericos = descripcion_perifericos;
+}
+public String getTipo_perifericos() {
+	return tipo_perifericos;
+}
+public void setTipo_perifericos(String tipo_perifericos) {
+	this.tipo_perifericos = tipo_perifericos;
+}
+}
