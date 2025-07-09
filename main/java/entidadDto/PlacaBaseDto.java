b1725258ed29c106950a65dfcfff79fae2c37ada
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-08 22:13:18 -0500

    creacion de la entidad PlacaBase dto

---
+++ main/java/entidadDto/PlacaBaseDto.java
@@ -0,0 +1,36 @@
+package entidadDto;
+
+public class PlacaBaseDto {
+private Integer id_placa_base, precio_placa_base, stock_placa_base;
+private String descripcion_placa_base, ram_generacion_placa_base;
+public Integer getId_placa_base() {
+	return id_placa_base;
+}
+public void setId_placa_base(Integer id_placa_base) {
+	this.id_placa_base = id_placa_base;
+}
+public Integer getPrecio_placa_base() {
+	return precio_placa_base;
+}
+public void setPrecio_placa_base(Integer precio_placa_base) {
+	this.precio_placa_base = precio_placa_base;
+}
+public Integer getStock_placa_base() {
+	return stock_placa_base;
+}
+public void setStock_placa_base(Integer stock_placa_base) {
+	this.stock_placa_base = stock_placa_base;
+}
+public String getDescripcion_placa_base() {
+	return descripcion_placa_base;
+}
+public void setDescripcion_placa_base(String descripcion_placa_base) {
+	this.descripcion_placa_base = descripcion_placa_base;
+}
+public String getRam_generacion_placa_base() {
+	return ram_generacion_placa_base;
+}
+public void setRam_generacion_placa_base(String ram_generacion_placa_base) {
+	this.ram_generacion_placa_base = ram_generacion_placa_base;
+}
+}
