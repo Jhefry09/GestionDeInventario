3790e99612d95914d47205a5e0e3d8b653bc8a81
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-09 15:30:18 -0500

    modifique el valor de String a integer en la generacion

--- main/java/entidadDto/NvidiaGraficaDto.java
+++ main/java/entidadDto/NvidiaGraficaDto.java
@@ -1,8 +1,14 @@
 package entidadDto;
 
 public class NvidiaGraficaDto {
-	private Integer id_nvidia_grafica, gama_nvidia_grafica, precio_nvidia_grafica, stock_nvidia_grafica;
-	private String descripcion_nvidia_grafica, generacion_nvidia_grafica;
+	private Integer id_nvidia_grafica, gama_nvidia_grafica, precio_nvidia_grafica, stock_nvidia_grafica, generacion_nvidia_grafica;
+	private String descripcion_nvidia_grafica;
+	public Integer getGeneracion_nvidia_grafica() {
+		return generacion_nvidia_grafica;
+	}
+	public void setGeneracion_nvidia_grafica(Integer generacion_nvidia_grafica) {
+		this.generacion_nvidia_grafica = generacion_nvidia_grafica;
+	}
 	public Integer getId_nvidia_grafica() {
 		return id_nvidia_grafica;
 	}
@@ -32,11 +38,5 @@ public class NvidiaGraficaDto {
 	}
 	public void setDescripcion_nvidia_grafica(String descripcion_nvidia_grafica) {
 		this.descripcion_nvidia_grafica = descripcion_nvidia_grafica;
-	}
-	public String getGeneracion_nvidia_grafica() {
-		return generacion_nvidia_grafica;
-	}
-	public void setGeneracion_nvidia_grafica(String generacion_nvidia_grafica) {
-		this.generacion_nvidia_grafica = generacion_nvidia_grafica;
 	}
 }
