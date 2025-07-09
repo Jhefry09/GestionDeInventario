17286dd04667443233f23b7e0f7674b72182f268
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-08 22:13:53 -0500

    creacion de la entidad Ram dto

---
+++ main/java/entidadDto/RamDto.java
@@ -0,0 +1,42 @@
+package entidadDto;
+
+public class RamDto {
+private Integer id_ram, capacidad_ram, precio_ram, stock_ram;
+private String descripcion_ram, ram_generacion_ram;
+public Integer getId_ram() {
+	return id_ram;
+}
+public void setId_ram(Integer id_ram) {
+	this.id_ram = id_ram;
+}
+public Integer getCapacidad_ram() {
+	return capacidad_ram;
+}
+public void setCapacidad_ram(Integer capacidad_ram) {
+	this.capacidad_ram = capacidad_ram;
+}
+public Integer getPrecio_ram() {
+	return precio_ram;
+}
+public void setPrecio_ram(Integer precio_ram) {
+	this.precio_ram = precio_ram;
+}
+public Integer getStock_ram() {
+	return stock_ram;
+}
+public void setStock_ram(Integer stock_ram) {
+	this.stock_ram = stock_ram;
+}
+public String getDescripcion_ram() {
+	return descripcion_ram;
+}
+public void setDescripcion_ram(String descripcion_ram) {
+	this.descripcion_ram = descripcion_ram;
+}
+public String getRam_generacion_ram() {
+	return ram_generacion_ram;
+}
+public void setRam_generacion_ram(String ram_generacion_ram) {
+	this.ram_generacion_ram = ram_generacion_ram;
+}
+}
