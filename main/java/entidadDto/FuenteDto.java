64645fcc62888813070d32cbb055dd57eb9c4b27
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-08 22:11:44 -0500

    creacion de la entidad Fuente dto

---
+++ main/java/entidadDto/FuenteDto.java
@@ -0,0 +1,42 @@
+package entidadDto;
+
+public class FuenteDto {
+private Integer id_fuente, watts_fuente, precio_fuente, stock_fuente;
+private String descripcion_fuente, certificacion_fuente;
+public Integer getId_fuente() {
+	return id_fuente;
+}
+public void setId_fuente(Integer id_fuente) {
+	this.id_fuente = id_fuente;
+}
+public Integer getWatts_fuente() {
+	return watts_fuente;
+}
+public void setWatts_fuente(Integer watts_fuente) {
+	this.watts_fuente = watts_fuente;
+}
+public Integer getPrecio_fuente() {
+	return precio_fuente;
+}
+public void setPrecio_fuente(Integer precio_fuente) {
+	this.precio_fuente = precio_fuente;
+}
+public Integer getStock_fuente() {
+	return stock_fuente;
+}
+public void setStock_fuente(Integer stock_fuente) {
+	this.stock_fuente = stock_fuente;
+}
+public String getDescripcion_fuente() {
+	return descripcion_fuente;
+}
+public void setDescripcion_fuente(String descripcion_fuente) {
+	this.descripcion_fuente = descripcion_fuente;
+}
+public String getCertificacion_fuente() {
+	return certificacion_fuente;
+}
+public void setCertificacion_fuente(String certificacion_fuente) {
+	this.certificacion_fuente = certificacion_fuente;
+}
+}
