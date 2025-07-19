6d8177c8b83e3597b28d7aaea6030fcfc52a871e
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-18 23:08:50 -0500

    cambie el nombre de la tabla para que todas tengan el mismo nombre, e hice un metodo para mostrar los datos

--- main/java/controlador/ServletInventario.java
+++ main/java/controlador/ServletInventario.java
@@ -57,17 +57,51 @@ public class ServletInventario extends HttpServlet {
 	}
 	protected void BtnEditar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Optional<String> Codigo = Optional.of(request.getParameter("idEditar")); 
-		Optional<String> tipoSelec = Optional.of("id"); 		
+		Optional<String> tipoSelec = Optional.of("id");
+		String IdSelec = request.getParameter("IdSelec");
 		String nomTa = request.getParameter("NomTabla");
-		switch (nomTa) {
-		case "Almacenamiento" -> {
-			request.setAttribute("datos", inventario.tAlmacenamiento("a.id_almac", Codigo, tipoSelec));		
-			System.out.println("paso por aqui");
-		}
-		default ->System.out.println("fallo, no encontro el nombre de la tabla a editar");
-		}
+		String titulo = mostrarDatos(nomTa, IdSelec, Codigo, tipoSelec, request);
 		request.setAttribute("nomTa", nomTa);
-		request.getRequestDispatcher("Agregar.jsp").forward(request, response);
+		request.setAttribute("titulo", titulo);
+		request.getRequestDispatcher("edicion.jsp").forward(request, response);
+	}
+	protected String mostrarDatos(String nombre, String id, Optional<String> Codigo, Optional<String> tipoSelec, HttpServletRequest request) {
+		String nombreTabla = switch (nombre) {
+		case "GestionDeInventario.Almacenamiento" ->{
+			request.setAttribute("datos", inventario.tAlmacenamiento(id, Codigo, tipoSelec));
+			yield "ALMACENAMIENTO";}
+		case "GestionDeInventario.Grafica_amd" ->{
+			request.setAttribute("datos", inventario.tAmdGrafica(id, Codigo, tipoSelec));
+			yield "GRAFICAS AMD";}
+		case "GestionDeInventario.ProcesadorAMD"-> {
+			request.setAttribute("datos", inventario.tAmdProcesador(id, Codigo, tipoSelec));
+			yield "PROCESADORES AMD";}
+		case "GestionDeInventario.Disipador"->{
+			request.setAttribute("datos", inventario.tDisipador(id, Codigo, tipoSelec));
+			yield "DISIPADORES";}
+		case "GestionDeInventario.Fuente_de_Energia"->{
+			request.setAttribute("datos", inventario.tFuente(id, Codigo, tipoSelec));
+			yield "FUENTES DE ENERGIA";}
+		case "GestionDeInventario.ProcesadorIntel"->{
+			request.setAttribute("datos", inventario.tIntelProcesador(id, Codigo, tipoSelec));
+			yield "PROCESADORES INTEL";}
+		case "GestionDeInventario.Grafica_nvidia"->{
+			request.setAttribute("datos", inventario.tNvidiaGrafica(id, Codigo, tipoSelec));
+			yield "GRAFICAS NVIDIA";}
+		case "GestionDeInventario.Perifericos"->{
+			request.setAttribute("datos", inventario.tPerifericos(id, Codigo, tipoSelec));
+			yield "PERIFERICOS";}
+		case "GestionDeInventario.PlacaBase"->{
+			request.setAttribute("datos", inventario.tPlacaBase(id, Codigo, tipoSelec));
+			yield "PLACAS BASE";}
+		case "GestionDeInventario.Ram"->{
+			request.setAttribute("datos", inventario.tRam(id, Codigo, tipoSelec));
+			yield "RAM";}
+		default ->{
+			yield "fallo, no encontro el nombre de la tabla a editar";}
+		};
+		System.out.println(nombreTabla);
+		return nombreTabla;
 	}
 	protected void Listar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	String inven = request.getParameter("inventario");
@@ -75,42 +109,8 @@ public class ServletInventario extends HttpServlet {
 	request.setAttribute("nombreSelec", "DESCRIPCION");
 	request.setAttribute("tipoSelec", "nom");
 	Optional<String> vacio = Optional.empty();
-		String nombreTabla = switch (inven) {
-			case "ALMACENAMIENTO" ->{
-				request.setAttribute("datos", inventario.tAlmacenamiento(null, vacio, vacio));
-				yield "ALMACENAMIENTO";}
-			case "AmdGra" ->{
-				request.setAttribute("datos", inventario.tAmdGrafica(null, vacio, vacio));
-				yield "GRAFICAS AMD";}
-			case "AmdPro"-> {
-				request.setAttribute("datos", inventario.tAmdProcesador(null, vacio, vacio));
-				yield "PROCESADORES AMD";}
-			case "DISIPADORES"->{
-				request.setAttribute("datos", inventario.tDisipador(null, vacio, vacio));
-				yield "DISIPADORES";}
-			case "FUENTES"->{
-				request.setAttribute("datos", inventario.tFuente(null, vacio, vacio));
-				yield "FUENTES DE ENERGIA";}
-			case "Intel"->{
-				request.setAttribute("datos", inventario.tIntelProcesador(null, vacio, vacio));
-				yield "PROCESADORES INTEL";}
-			case "Nvidia"->{
-				request.setAttribute("datos", inventario.tNvidiaGrafica(null, vacio, vacio));
-				yield "GRAFICAS NVIDIA";}
-			case "PERIFERICOS"->{
-				request.setAttribute("datos", inventario.tPerifericos(null, vacio, vacio));
-				yield "PERIFERICOS";}
-			case "PlacaBase"->{
-				request.setAttribute("datos", inventario.tPlacaBase(null, vacio, vacio));
-				yield "PLACAS BASE";}
-			case "RAM"->{
-				request.setAttribute("datos", inventario.tRam(null, vacio, vacio));
-				yield "RAM";}
-			default ->{
-				System.out.println("Fallo");
-				yield "FALLO";}
-			};
-	request.setAttribute("nombre", nombreTabla);
+	String titulo = mostrarDatos(inven, null, vacio, vacio, request);
+	request.setAttribute("nombre", titulo);
 	request.getRequestDispatcher("inventario.jsp").forward(request, response);
 	}
 	protected String TipoBuscar(String id, String precio, String descripcion, HttpServletRequest request ) {
@@ -140,52 +140,52 @@ public class ServletInventario extends HttpServlet {
 		request.setAttribute("nombreSelec", seleccionado);
 		
 		String nombre = switch (inven) {
-		case "ALMACENAMIENTO"->{
+		case "GestionDeInventario.Almacenamiento"->{
 			tipoBuscar = TipoBuscar("a.id_almac", "a.precio_almac", "a.descrip_almace", request);
 			request.setAttribute("datos", inventario.tAlmacenamiento(tipoBuscar, datos, tipoSelec));
 			yield "ALMACENAMIENTO";
 		}
-		case "AmdGra"->{
+		case "GestionDeInventario.Grafica_amd"->{
 			tipoBuscar = TipoBuscar("ga.id_grafica_amd", "ga.precio_grafi_amd", "ga.descripcion_grafi_amd", request);
 			request.setAttribute("datos", inventario.tAmdGrafica(tipoBuscar, datos, tipoSelec));
 			yield "GRAFICAS AMD";
 		}
-		case "AmdPro"->{
+		case "GestionDeInventario.ProcesadorAMD"->{
 			tipoBuscar = TipoBuscar("pa.id_proce_AMD ", "pa.precio_proce_AMD", "gp.nom_gama_proc_amd", request);
 			request.setAttribute("datos", inventario.tAmdProcesador(tipoBuscar, datos, tipoSelec));
 			yield "PROCESADORES AMD";
 		}
-		case "DISIPADORES"->{
+		case "GestionDeInventario.Disipador"->{
 			tipoBuscar = TipoBuscar("d.id_disipador", "d.precio_disipador", "d.descrip_disip", request);
 			request.setAttribute("datos", inventario.tDisipador(tipoBuscar, datos, tipoSelec));
 			yield "DISIPADORES";
 		}
-		case "FUENTES"->{
+		case "GestionDeInventario.Fuente_de_Energia"->{
 			tipoBuscar = TipoBuscar("f.id_fuente", "f.precio_fuente", "f.descripcion_fuente", request);
 			request.setAttribute("datos", inventario.tFuente(tipoBuscar, datos, tipoSelec));
 			yield "FUENTES DE ENERGIA";
 		}
-		case "Intel"->{
+		case "GestionDeInventario.ProcesadorIntel"->{
 			tipoBuscar = TipoBuscar("pi.id_proce_intel", "pi.precio_proce_intel", "gi.nom_gama_intel", request);
 			request.setAttribute("datos", inventario.tIntelProcesador(tipoBuscar, datos, tipoSelec));
 			yield "PROCESADORES INTEL";
 		}
-		case "Nvidia"->{
+		case "GestionDeInventario.Grafica_nvidia"->{
 			tipoBuscar = TipoBuscar("gn.id_grafica_nvidia", "gn.precio_grafi_nvidia", "gn.descripcion_grafi_nvidia", request);
 			request.setAttribute("datos", inventario.tNvidiaGrafica(tipoBuscar, datos, tipoSelec));
 			yield "GRAFICAS NVIDIA";
 		}
-		case "PERIFERICOS"->{
+		case "GestionDeInventario.Perifericos"->{
 			tipoBuscar = TipoBuscar("p.id_perifericos", "p.precio_perifericos", "p.descripc_perifericos", request);
 			request.setAttribute("datos", inventario.tPerifericos(tipoBuscar, datos, tipoSelec));
 			yield "PERIFERICOS";
 		}
-		case "PlacaBase"->{
+		case "GestionDeInventario.PlacaBase"->{
 			tipoBuscar = TipoBuscar("pb.id_placa_base", "pb.precio_placa_base", "pb.descri_placa_base", request);
 			request.setAttribute("datos", inventario.tPlacaBase(tipoBuscar, datos, tipoSelec));
 			yield "PLACAS BASE";
 		}
-		case "RAM"->{
+		case "GestionDeInventario.Ram"->{
 			tipoBuscar = TipoBuscar("r.id_ram", "r.precio_ram", "r.descrip_ram", request);
 			request.setAttribute("datos", inventario.tRam(tipoBuscar, datos, tipoSelec));
 			yield "RAM";
@@ -200,19 +200,7 @@ public class ServletInventario extends HttpServlet {
 	protected void BtnBorrar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Integer idBorrar = Integer.parseInt(request.getParameter("codigo"));
 		String nomTabla = request.getParameter("NomTabla");
-		String tipoId = switch (nomTabla) {
-		case "GestionDeInventario.Ram" -> "id_ram";
-		case "GestionDeInventario.PlacaBase" -> "id_placa_base";
-		case "GestionDeInventario.Perifericos" -> "id_perifericos";
-		case "GestionDeInventario.Grafica_nvidia" -> "id_grafica_nvidia";
-		case "GestionDeInventario.Grafica_amd" -> "id_grafica_amd";
-		case "GestionDeInventario.ProcesadorAMD" -> "id_proce_AMD";
-		case "GestionDeInventario.ProcesadorIntel" -> "id_proce_intel";
-		case "GestionDeInventario.Fuente_de_Energia" -> "id_fuente";
-		case "GestionDeInventario.Disipador" -> "id_disipador";
-		case "GestionDeInventario.Almacenamiento" -> "id_almac";
-		default -> "FALLO";
-		};
+		String tipoId = request.getParameter("IdSelec");
 		System.out.println(idBorrar);
 		System.out.println(nomTabla);
 		System.out.println(tipoId);
